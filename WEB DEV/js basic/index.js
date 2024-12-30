04ed60575da0ab0f240fcd556394066120957c97
sagar1234gupta <sagar.gupta_cs22@gla.ac.in>
2024-12-30 08:19:39 +0530

    Add files via upload

---
+++ WEB DEV/js basic/index.js
@@ -0,0 +1,21 @@
+ //line bleow is used to print on the console window
+ console.log('Namaste duniya version 10' );
+
+//  let a = 'true';
+//  console.log(a);
+
+//  let lastName = 'Sagar';
+//  console.log(lastName);
+
+//  lastName = 3;
+//  console.log(lastName)
+
+//  let b = 12;
+//  let c = 5;
+//  let d = b%c;
+//  console.log(d)
+
+// console.log( 3 === 3);
+
+// 
+ 
