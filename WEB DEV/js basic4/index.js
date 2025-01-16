d04a9644e6b37dd511624b0679ac1985a473d6fe
sagar1234gupta <sagar.gupta_cs22@gla.ac.in>
2025-01-16 23:36:09 +0530

    Add files via upload

---
+++ WEB DEV/js basic4/index.js
@@ -0,0 +1,105 @@
+// console.log('hello jee');
+
+
+//  function call or invite
+// run();
+
+//  function declaration
+// function run() {
+//     console.log('running');
+
+// }
+
+// function assignment
+
+// let stand = function walk(){
+//     console.log('walking');
+// };
+
+// stand();
+
+// let jump = stand;
+// jump();
+
+
+// let x = 1;
+// x = 'a';
+
+// console.log(x);
+
+
+
+// function sum(a,b){
+//     console.log(arguments);
+
+// return a+b;
+// }
+
+// console.log(sum(1,2));
+
+
+// let ans = sum(1,2,3,4,5,6,7,8,9,0);
+
+// function sum(num,value,...args){
+//     console.log(args);
+// }
+
+// sum(1,2,3,4,5,6,sum);
+
+
+// defoult parameters
+
+// function interest(p,r,y){
+//     return p*r*y/100;
+// }
+// console.log(interest(1000, 10, 5))
+
+// let person = {
+
+//  fName : 'sagar',
+//  lName: 'Gupta',
+//  get fullName(){
+   
+//     return `${person.fName}   ${person.lName}`;
+//  },
+// set fullName(value){
+//     if(typeof value !== 'string'){
+//         throw new Error("you have not sent a string")
+//     }
+//     let parts = value.split('');
+//     this.fName = parts[0];
+//     this.Name = parts[1];
+// }
+
+// };
+ 
+
+// console.log(person);
+
+        
+
+
+//  read only
+
+
+// console.log(fulNmae());
+
+// try{
+//     person.fullName = true;
+// }
+// catch(e){
+//     alert(e);
+// }
+// console.log(person.fulNmae);
+
+// {
+//     var a = 5;
+// }
+// console.log(a);
+
+// function walk(){
+//     var a = 5;
+// }
+
+
+
